84f0ab2f0467982c6df024dc960fd4173822ba62
12156C <1745680380@qq.com>
2022-12-16 23:33:52 +0800

    补充project-7

---
+++ project-7/myapp/src/cn/edu/swu/user/LogoutServlet.java
@@ -0,0 +1,20 @@
+package cn.edu.swu.user;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session=request.getSession();
+        if(session!=null){
+            session.invalidate();
+        }
+        response.sendRedirect("./index.html");
+    }
+}
